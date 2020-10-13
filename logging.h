2a597a47a07711c736c449d55c3bb7e69fbb439d
Sasszem <barath.laszlo.szolnok@gmail.com>
2020-10-13 20:31:20 +0200

    Refactor logging a bit

--- logging.h
+++ logging.h
@@ -1,13 +1,14 @@
-
 #ifdef LOGGING
 
 #define LOG(...) printf("\e[33mLOG:\e[39m\t"); printf(__VA_ARGS__)
 #define LOGC(...) printf(__VA_ARGS__);
+#define LOGDO(x) x;
 
 #else
 #define LOG(...) 
 #define LOGC(...)
+#define LOGDO(x)
 #endif
 
-#define ERROR(...) printf("\e[41mERROR\e[49m: "); printf(__VA_ARGS__)
+#define ERROR(...) printf("\e[41mERROR\e[49m:\t"); printf(__VA_ARGS__)
 #define ERRORC(...) printf(__VA_ARGS__);
