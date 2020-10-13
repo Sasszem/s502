533c1620f34592b363bef9526fa0d5122ef8847d
Sasszem <barath.laszlo.szolnok@gmail.com>
2020-10-14 00:42:27 +0200

    Add ifdef directive

--- preprocess.c
+++ preprocess.c
@@ -48,8 +48,9 @@ int process_define(State *s, Token t) {
 enum PPCommand {
     PPC_STOP = -1,
     PPC_NOP = 0,
-    PPC_INC_IF_LVL,
-    PPC_DEC_IF_LVL,
+    PPC_IF_TRUE,
+    PPC_IF_FALSE,
+    PPC_ENDIF,
 };
 
 enum PPCommand process_include(State *s, Token t) {
@@ -58,11 +59,47 @@ enum PPCommand process_include(State *s, Token t) {
     return PPC_STOP;
 }
 
-enum PPCommand do_preprocessor_token(State *s, Token t) {
+enum PPCommand process_ifdef(State *s, Token t) {
+    char *cmd = t.stripped+1;
+    char *val = util_find_string_segment(cmd) + 1;
+    if (*(val-1)!=' ') {
+        ERROR("Too few arguments for ifdef!");
+        token_print(&t);
+        return PPC_STOP;
+    }
+    char *vend = util_find_string_segment(val);
+    if (*vend!='\0'){
+        ERROR("Too many arguments for ifdef!");
+        token_print(&t);
+        return PPC_STOP;
+    }
+    char name[DEFINE_MAX_LEN];
+    strncpy(name, val, vend-val);
+    name[vend-val] = 0;
+    if (map_get(&(s->defines), name)!=-1) {
+        return PPC_IF_TRUE;
+    }
+    return PPC_IF_FALSE;
+}
+
+enum PPCommand do_preprocessor_token(State *s, Token t, int skip) {
     LOG("Processing preproessor token:\n");
     LOGDO(token_print(&t));
     
     char *f = t.stripped + 1;
+
+    if (strncmp(t.stripped+1, "ifdef", strlen("ifdef"))==0) {
+        if (skip)
+            return PPC_IF_TRUE;
+        return process_ifdef(s, t);
+    }
+
+    if (strncmp(t.stripped+1, "endif", strlen("endif"))==0) {
+        return PPC_ENDIF;
+    }
+
+    if (skip)
+        return PPC_NOP;
 
     if (strncmp(t.stripped+1, "define", strlen("define"))==0) {
         //LOG("Found a define...\n");
@@ -71,6 +108,7 @@ enum PPCommand do_preprocessor_token(State *s, Token t) {
     if (strncmp(t.stripped+1, "include", strlen("include"))==0) {
         return process_include(s, t)<0 ? PPC_STOP : PPC_NOP;
     }
+    
     ERROR("Unknown preprocessor directive: %s\n", f);
     return PPC_STOP;
 }
@@ -83,27 +121,43 @@ enum PPCommand do_preprocessor_token(State *s, Token t) {
 int preprocess(State *s, TokensList *tokens) {
     TokensListElement *ptr = tokens->head;
     int iflevel = 0;
-    int skip = 0;
+    int skip = 0, skiponce = 0;
+    enum PPCommand p;
     while (ptr!=NULL) {
         Token t = ptr->token;
         if (t.type == TT_PREPROC) {
-            skip = 1;
+            p = do_preprocessor_token(s, t, skip);
 
-            enum PPCommand p = do_preprocessor_token(s, t);
-            
-            if (p==PPC_INC_IF_LVL)
+            if (p==PPC_IF_TRUE) {
                 iflevel++;
-            if (p==PPC_DEC_IF_LVL)
-                iflevel = iflevel==0 ? 0 : iflevel-1;
+                skiponce = 1;
+            }
+            if (p==PPC_IF_FALSE) {
+                iflevel++;
+                skiponce = 1;
+                skip = 1;
+            }
+            if (p==PPC_ENDIF) {
+                iflevel--;
+                skiponce = 1;
+                if (iflevel<0) {
+                    ERROR("More endif's than if's!\n");
+                    token_print(&t);
+                    return -1;
+                }
+                if (iflevel==0) {
+                    skip =0;
+                }
+            }
             if (p==PPC_STOP)
                 return -1;
         }
-
-        ptr = ptr->next;
-        if (skip==1 || iflevel>0){
-            tokenslist_remove(tokens, ptr->prev);
-        } 
-        skip = 0;
+        if (skip||skiponce){
+            ptr = tokenslist_remove(tokens, ptr);
+            skiponce = 0;
+        } else {
+            ptr = ptr->next;
+        }
     }
     return 0;
 }
