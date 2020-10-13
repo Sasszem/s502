e127f8df3b77caf94897abd8897fd1a8278e103e
Sasszem <barath.laszlo.szolnok@gmail.com>
2020-10-14 00:44:22 +0200

    ifdef example

--- test.asm
+++ test.asm
@@ -1,40 +1,37 @@
 lda #5 ; 5-ot mint konstans A-ba
-sta $400 ; eltaroljuk 400-ba
-lda $8 ; 8-as ZPG cimet A-ba
-sta $9 ; elmentjuk 9-be (zpg)
+;sta $400 ; eltaroljuk 400-ba
+;lda $8 ; 8-as ZPG cimet A-ba
+;sta $9 ; elmentjuk 9-be (zpg)
 ; begin a loop
 ; init
-ldx #0
-lda #$23
-.define SCREEN $400
-.define ASD #4
-.define B #$10
-.define C @B
+;ldx #0
+;lda #$23
+;.define SCREEN $400
+;.define ASD #4
+;.define B #$10
+;.define C @B
 ;.include "c64.asm"
 .ifdef A
-.print Defining ASD
-.define ASD
+;.print Defining ASD
+.define ASD 5
+.ifdef ASD
+.define C 9
+.endif
+.endif
+
+.define A 5
+.ifdef A
+    .define B 8
+    .ifdef B
+        .define C 9
+    .endif
+    .ifdef Q
+        .define W 9
+    .endif
 .endif
 ; loop body
-loop:
-    sta %SCREEN, X
-    inx
-    cpx 10
-    bne loop ; loop until x is 10
-nem
-opk
-odF
-kib
-asz
-ott
-hos
-szu
-sor
-ami
-kia
-kas
-ztj
-aaf
-ord
-ito
-tis
+;loop:
+;    sta %SCREEN, X
+;    inx
+;    cpx 10
+;    bne loop ; loop until x is 10
