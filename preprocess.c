76699af74c23ae506f4832f2ba5c380d830973f0
Sasszem <barath.laszlo.szolnok@gmail.com>
2020-10-25 15:19:56 +0100

    Refactor preprocess Change type of token processor functions Needed to add include (needs the list to do an insert)

--- preprocess.c
+++ preprocess.c
@@ -1,4 +1,6 @@
 #include <string.h>
+#include <stdlib.h>
+#include "debugmalloc.h"
 
 #include "state.h"
 #include "token.h"
@@ -6,6 +8,7 @@
 #include "preprocess.h"
 #include "util.h"
 #include "number.h"
+#include "loadfile.h"
 
 enum PPCommand {
     PPC_STOP = -1,
@@ -15,31 +18,31 @@ enum PPCommand {
     PPC_ENDIF,
 };
 
-typedef enum PPCommand(*tokenprocessor)(State *, Token);
+typedef enum PPCommand(*tokenprocessor)(State *, TokensList *, TokensListElement*);
 
 
-enum PPCommand process_define(State *s, Token t) {
+enum PPCommand process_define(State *s, TokensList *list, TokensListElement *ptr) {
     // find string
-    char *define = t.stripped + 1;
+    char *define = ptr->token.stripped + 1;
     
     char *name = util_find_string_segment(define) + 1;
     if (*(name-1)=='\0') {
         ERROR("Not enough args to define!\n");
-        token_print(&t);
+        token_print(&(ptr->token));
         return PPC_STOP;
     }
 
     char *num = util_find_string_segment(name) + 1;
     if (*(num-1)=='\0') {
         ERROR("Not enough args to define!\n");
-        token_print(&t);
+        token_print(&(ptr->token));
         return PPC_STOP;
     }
 
     char *nend = util_find_string_segment(num);
     if (*nend!='\0') {
         ERROR("Too many args to define!\n");
-        token_print(&t);
+        token_print(&(ptr->token));
         return PPC_STOP;
     }
 
@@ -57,28 +60,28 @@ enum PPCommand process_define(State *s, Token t) {
 }
 
 
-enum PPCommand process_ifbeq(State *s, Token t) {
+enum PPCommand process_ifbeq(State *s, TokensList *list, TokensListElement *ptr) {
     // find string
-    char *ifbeq = t.stripped + 1;
+    char *ifbeq = ptr->token.stripped + 1;
     
     char *first = util_find_string_segment(ifbeq) + 1;
     if (*(first-1)=='\0') {
         ERROR("Not enough args to ifbeq!\n");
-        token_print(&t);
+        token_print(&(ptr->token));
         return PPC_STOP;
     }
 
     char *second = util_find_string_segment(first) + 1;
     if (*(second-1)=='\0') {
         ERROR("Not enough args to ifbeq!\n");
-        token_print(&t);
+        token_print(&(ptr->token));
         return PPC_STOP;
     }
 
     char *send = util_find_string_segment(second);
     if (*send!='\0') {
         ERROR("Too many args to ifbeq!\n");
-        token_print(&t);
+        token_print(&(ptr->token));
         return PPC_STOP;
     }
 
@@ -88,36 +91,37 @@ enum PPCommand process_ifbeq(State *s, Token t) {
     int first_as_num = number_get_number(s, first, second-first-1);
     if (first_as_num<0) {
         ERROR("ifbeq first argument not defined!\n");
-        token_print(&t);
+        token_print(&(ptr->token));
         return PPC_STOP;
     }
     int second_as_num = number_get_number(s, second, send-second);
     if (second_as_num<0) {
         ERROR("ifbeq second argument not defined!\n");
-        token_print(&t);
+        token_print(&(ptr->token));
         return PPC_STOP;
     }
     return first_as_num>second_as_num ? PPC_IF_TRUE : PPC_IF_FALSE;
 }
 
-enum PPCommand process_endif(State *s, Token t){
+enum PPCommand process_endif(State *s, TokensList *list, TokensListElement *ptr){
     return PPC_ENDIF;
 }
 
-enum PPCommand process_print(State *s, Token t) {
-    char *str = &t.stripped[1];
+enum PPCommand process_print(State *s, TokensList *list, TokensListElement *ptr) {
+    char *str = &(ptr->token.stripped[1]);
     str = util_find_string_segment(str) + 1;
     printf("\e[44mMESSAGE\e[49m\t%s\n", str);
     return PPC_NOP;
 }
 
-enum PPCommand process_printc(State *s, Token t) {
-    char *str = &t.stripped[1];
+enum PPCommand process_printc(State *s, TokensList *list, TokensListElement *ptr) {
+    char *str = &(ptr->token.stripped[1]);
     str = util_find_string_segment(str) + 1;
     char *send = util_find_string_segment(str);
 
     if (*send!='\0') {
         ERROR("Too many arguemnts to printc!\n");
+        token_print(&(ptr->token));
         return PPC_STOP;
     }
 
@@ -134,23 +138,49 @@ enum PPCommand process_printc(State *s, Token t) {
     return PPC_NOP;
 }
 
-enum PPCommand process_include(State *s, Token t) {
-    ERROR("INCLUDE NOT IMPLEMENTED YET!\n");
-    return PPC_STOP;
+enum PPCommand process_include(State *s, TokensList *list, TokensListElement *ptr) {
+    char *str = &(ptr->token.stripped[1]);
+    str = util_find_string_segment(str) + 1;
+    if (*(str-1)=='\0') {
+        ERROR("Too few argumenst to include!\n");
+        token_print(&(ptr->token));
+        return PPC_STOP;
+    }
+    char *send = util_find_string_segment(str);
+    if (*send!='\0') {
+        ERROR("Too many arguments to include!\n");
+        token_print(&(ptr->token));
+        return PPC_STOP;
+    }
+    LOG("Including '%.*s'\n", (int)(send-str), str);
+    char name[FILENAME_MAX];
+    strncpy(name, str, send-str);
+    TokensList *f = load_file(name);
+    if (f==NULL) {
+        ERROR("Could not include file!\n");
+        token_print(&(ptr->token));
+        return PPC_STOP;
+    }
+    tokenslist_insert(list, ptr, f);
+    ERROR("That file:");
+    tokenslist_debug_print(f);
+    tokenslist_delete(f);
+    free(f);
+    return PPC_NOP;
 }
 
-enum PPCommand process_ifdef(State *s, Token t) {
-    char *cmd = t.stripped+1;
+enum PPCommand process_ifdef(State *s, TokensList *list, TokensListElement *ptr) {
+    char *cmd = &(ptr->token.stripped[1]);
     char *val = util_find_string_segment(cmd) + 1;
     if (*(val-1)!=' ') {
         ERROR("Too few arguments for ifdef!");
-        token_print(&t);
+        token_print(&(ptr->token));
         return PPC_STOP;
     }
     char *vend = util_find_string_segment(val);
     if (*vend!='\0'){
         ERROR("Too many arguments for ifdef!");
-        token_print(&t);
+        token_print(&(ptr->token));
         return PPC_STOP;
     }
     char name[DEFINE_MAX_LEN];
@@ -162,8 +192,8 @@ enum PPCommand process_ifdef(State *s, Token t) {
     return PPC_IF_FALSE;
 }
 
-enum PPCommand process_ifndef(State *s, Token t) {
-    enum PPCommand ret = process_ifdef(s, t);
+enum PPCommand process_ifndef(State *s, TokensList *list, TokensListElement *ptr) {
+    enum PPCommand ret = process_ifdef(s, list, ptr);
     if (ret==PPC_IF_FALSE)
         return PPC_IF_TRUE;
     if (ret==PPC_IF_TRUE)
@@ -189,11 +219,11 @@ struct {enum PPCommand ret; char *name;} skipProcessors[] = {
     { PPC_IF_TRUE,  "ifbeq" },
 };
 
-enum PPCommand do_preprocessor_token(State *s, Token t, int skip) {
+enum PPCommand do_preprocessor_token(State *s, TokensList *list, TokensListElement *ptr, int skip) {
     LOG("Processing preproessor token:\n");
-    LOGDO(token_print(&t));
+    LOGDO(token_print(&(ptr->token)));
     
-    char *f = t.stripped + 1;
+    char *f = ptr->token.stripped + 1;
 
     if (skip) {
         for (int i = 0; i<sizeof(skipProcessors)/sizeof(skipProcessors[0]); i++) {
@@ -206,7 +236,7 @@ enum PPCommand do_preprocessor_token(State *s, Token t, int skip) {
 
     for (int i = 0; i<sizeof(processors)/sizeof(processors[0]); i++) {
         if (strncmp(f, processors[i].name, strlen(processors[i].name))==0) {
-            return processors[i].p(s, t);
+            return processors[i].p(s, list, ptr);
         }
     }
     
@@ -227,7 +257,7 @@ int preprocess(State *s, TokensList *tokens) {
     while (ptr!=NULL) {
         Token t = ptr->token;
         if (t.type == TT_PREPROC) {
-            p = do_preprocessor_token(s, t, skip);
+            p = do_preprocessor_token(s, tokens, ptr, skip);
             skiponce = 1;
             if (p==PPC_IF_TRUE) {
                 iflevel++;
