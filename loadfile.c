5f6f18f12f6528180e45f2f62507e95f8cc0ae50
Sasszem <barath.laszlo.szolnok@gmail.com>
2020-11-06 00:55:33 +0100

    Readibility improvements on loadfile

--- loadfile.c
+++ loadfile.c
@@ -18,27 +18,27 @@
  * - don't write ; and any characters after
  * - stop on EOF and \n
  */
-int read_token(FILE *f, Token *t) {
-    
-    // TODO: refactor this
+int read_token(FILE* f, Token* t) {
+
     int seen_whitespace = 1, eol = 0;
     int ptr, c;
+
     // -1 for trailing nullbyte
-    for (ptr = 0; ptr<TOKEN_BUFFER_SIZE-1; ) {
+    for (ptr = 0; ptr < TOKEN_BUFFER_SIZE - 1;) {
         // read char
         c = fgetc(f);
 
         // stop condition
-        if (c==EOF || c=='\n') {
+        if (c == EOF || c == '\n') {
             break;
         }
 
         // don't write if already reached a comment
         if (eol)
             continue;
-        
+
         // whitespace handling
-        if (c=='\t' || c==' ') {
+        if (c == '\t' || c == ' ') {
             if (!seen_whitespace) {
                 c = ' ';
                 seen_whitespace = 1;
@@ -49,7 +49,7 @@ int read_token(FILE *f, Token *t) {
             seen_whitespace = 0;
 
             // mark comment
-            if (c==';') {
+            if (c == ';') {
                 eol = 1;
                 continue;
             }
@@ -61,60 +61,70 @@ int read_token(FILE *f, Token *t) {
     }
 
     // too long line
-    if (ptr==TOKEN_BUFFER_SIZE-1) {
+    if (ptr == TOKEN_BUFFER_SIZE - 1) {
         t->len = ptr;
         return -1;
     }
 
     // kill trailing whitespace
-    if (t->stripped[ptr-1]==' ')
+    if (t->stripped[ptr - 1] == ' ')
         ptr--;
-    
+
     // zero-terminate
     t->stripped[ptr] = 0;
     // len
     t->len = ptr;
 
     // log
-    if (ptr>0) {
+    if (ptr > 0) {
         LOG("READ TOKEN:\n");
         LOGDO(token_print(t));
     }
 
     // EOF return
-    if (c==EOF)
+    if (c == EOF)
         return 0;
-    
+
     return 1;
 }
 
 /**
  * Parse token - test if it's an opcode, a label or a preprocessor statement
  */
-int recognize_token(Token *t) {
+int recognize_token(Token* t) {
+    // how many token types does it fit
     int found = 0;
-    if (t->stripped[0]=='.') {
+
+    // directive - starts with a dot
+    if (t->stripped[0] == '.') {
         t->type = TT_PREPROC;
         found++;
     }
-    if (t->stripped[t->len-1]==':') {
+
+    // label - ends with a ':'
+    if (t->stripped[t->len - 1] == ':') {
         t->type = TT_LABEL;
         found++;
     }
-    if (t->stripped[3]==' ' || t->stripped[3]=='\0') {
+
+    // instruction - 3rd char is a space or len is 3
+    if (t->stripped[3] == ' ' || t->stripped[3] == '\0') {
         t->type = TT_INSTR;
         found++;
     }
-    if (found!=1) {
+
+    // 0 or more than one match is a problem
+    if (found != 1) {
         ERROR("Can not recognize token:\n");
         token_print(t);
         return -1;
     }
+
     LOG("Recognized token as %d:\n", t->type);
     LOGDO(token_print(t));
-    return 0;
-} 
 
+    return 0;
+}
 
 /**
  * High-level func to read the contents from a file
@@ -124,29 +134,36 @@ int recognize_token(Token *t) {
  * - returns said token list
  * - returns NULL on error
  */
-TokensList* read_file(char *name) {
-    FILE *f = fopen(name, "r");
-    if (f==NULL) {
+TokensList* read_file(char* name) {
+
+    // basic file open
+    FILE* f = fopen(name, "r");
+    if (f == NULL) {
         ERROR("An error occured opening the file %s!\n", name);
-        ERROR("Error opening file: %s\n", strerror( errno ));
+        ERROR("Error opening file: %s\n", strerror(errno));
         return NULL;
     }
-    TokensList *tokenslist = tokenslist_new();
+
+    TokensList* tokenslist = tokenslist_new();
     Token tok;
     int lineno = 1;
     int res;
+
+    // reading tokens while possible
     do {
-        res=read_token(f, &tok);
-        if (tok.len>0) {
+        res = read_token(f, &tok);
+        if (tok.len > 0) {
             strncpy(tok.source.fname, name, TOKEN_SOURCE_FILE_SIZE);
             tok.source.lineno = lineno;
-            tokenslist_add(tokenslist ,tok);
+            tokenslist_add(tokenslist, tok);
         }
         lineno++;
-    } while (res>0);
+    } while (res > 0);
 
     fclose(f);
-    if (res<0) {
+
+    // return error if stopped due to reading error
+    if (res < 0) {
         ERROR("line is too long!\n");
         token_print(&tok);
         tokenslist_free(tokenslist);
@@ -159,9 +176,9 @@ TokensList* read_file(char *name) {
  * Do token recognition on all tokens in a list
  * Returns 0 on success, -1 on error
  */
-int recognize_tokens(TokensList *t) {
-    for(TokensListElement *ptr = t->head; ptr!=NULL; ptr = ptr->next)
-        if (recognize_token(&(ptr->token)) < 0){
+int recognize_tokens(TokensList* t) {
+    for (TokensListElement* ptr = t->head; ptr != NULL; ptr = ptr->next)
+        if (recognize_token(&(ptr->token)) < 0) {
             FAIL("Can not recognize token types!\n");
             return -1;
         }
@@ -171,7 +188,6 @@ int recognize_tokens(TokensList *t) {
 // this should not be here, or rather all the other function shoudl be elsewhere...
 // TODO: refactor this
 // maybe we can use a monadic pattern here?
-
 
 /**
  * High level function that reads a whole file
@@ -183,14 +199,21 @@ int recognize_tokens(TokensList *t) {
  */
 TokensList* load_file(char* name) {
     LOG("Reading file...\n");
-    TokensList *list = read_file(name);
-    if (list==NULL)
-        return NULL;
+
+    TokensList* list = read_file(name);
+    if (list == NULL)
+        goto ERROR;
+
     LOG("Running first analysis...\n");
-    if (recognize_tokens(list)<0) {
-        FAIL("Failed to load file contents: '%s'\n", name);
-        tokenslist_free(list);
-        return NULL;
-    }
+    if (recognize_tokens(list) < 0)
+        goto ERROR;
+
     return list;
+
+ERROR:
+    FAIL("Failed to load file contents: '%s'\n", name);
+    if (list != NULL) {
+        tokenslist_free(list);
+    }
+    return NULL;
 }
