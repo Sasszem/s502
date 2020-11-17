4fae8cc60da356b35b755256412f59c37726629c
Sasszem <barath.laszlo.szolnok@gmail.com>
2020-11-17 12:58:42 +0100

    expose token_link_instruction

--- tokenFunc.h
+++ tokenFunc.h
@@ -4,9 +4,10 @@
 
 #include "token_t.h"
 #include "instructions.h"
-
+#include "state.h"
 
 void token_print(Token *token);
 int token_get_addressmode(Token *t);
+int token_link_instruction(State *s, Token *token);
 
 #endif
