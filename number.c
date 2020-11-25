bca3ed9242d55c8f16bd8ada288d517bf997a284
Sasszem <barath.laszlo.szolnok@gmail.com>
2020-11-25 01:39:48 +0100

    Add label parsing to numbers module

--- number.c
+++ number.c
@@ -12,7 +12,7 @@ int is_valid_define_char(char c) {
 /**
  * Convert a hex char to a digit
  * Handles upper or lowercase chars
- * Returns -1 on failed conversion
+ * Returns NUMBER_ERROR on failed conversion
  */
 int number_char_to_digit(char c) {
     if ('0'<=c && c<='9')
@@ -21,13 +21,13 @@ int number_char_to_digit(char c) {
         c += 'A'-'a';
     if ('A'<=c && c<='F')
         return c-'A'+10;
-    return -1;
+    return NUMBER_ERROR;
 }
 
 /**
  * Parse a number from a string
  * Handles base marker $ symbol
- * Returns -1 on failed parsing
+ * Returns NUMBER_ERROR on failed parsing
  */
 int number_parse_number(char *str, int count) {
     int base = 10;
@@ -40,9 +40,9 @@ int number_parse_number(char *str, int count) {
     while (ptr<count) {
         num *= base;
         int digit = number_char_to_digit(str[ptr]);
-        if (digit==-1 || digit >=base) {
+        if (digit==NUMBER_ERROR || digit >=base) {
             ERROR("Can not interpret number: '%.*s'\n", count, str);
-            return -1;
+            return NUMBER_ERROR;
         }
         
         num += digit;
@@ -52,16 +52,17 @@ int number_parse_number(char *str, int count) {
 }
 
 /**
- * interpret a string as a constant or number
+ * interpret a string as a constant, label or number
  * - get the value of a constant
  * - parse number
- * Returns -1 on error
+ * Returns NUMBER_ERROR on error, or NUMBER_LABEL_NODEF if number is a valid label with no value
  */
 int number_get_number(State *s, char *str, int count) {
     int ptr = 0;
     if (str[ptr]=='#')
         ptr++;
     if (str[ptr]=='@') {
+        // constant parsing
         char number[MAP_MAX_KEY_LEN];
         strncpy(number, str+ptr+1, count-ptr-1);
         number[count-ptr-1] = 0;
@@ -72,6 +73,18 @@ int number_get_number(State *s, char *str, int count) {
         }
         return n;
     }
-    // pass error (-1 value)
+    if (str[ptr]=='&') {
+        // label parsing
+        char number[MAP_MAX_KEY_LEN];
+        strncpy(number, str+ptr+1, count-ptr-1);
+        number[count-ptr-1] = 0;
+        int n = map_get(s->labels, number);
+        if (n==-1) {
+            LOG(2, "Undefined label: %.*s\n", count-ptr-1, str+ptr+1);
+            return NUMBER_LABEL_NODEF;
+        }
+        return n;
+    }
+    // pass error (NUMBER_ERROR value)
     return number_parse_number(str+ptr, count-ptr);
 }
--- number.h
+++ number.h
@@ -2,6 +2,12 @@
 #define GUARD_NUMBER
 
 #include "state.h"
+
+enum {
+    NUMBER_ERROR = -1,
+    NUMBER_LABEL_NODEF = -2,
+};
+
 int number_get_number(State *s, char *str, int count);
 int number_parse_number(char *str, int count);
 int number_char_to_digit(char c);
