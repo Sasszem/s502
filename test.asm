358ba2b29a19eed800370af1ec844ef564221480
Sasszem <barath.laszlo.szolnok@gmail.com>
2020-11-25 02:05:54 +0100

    Add super-simple example Now we can even compile it!

--- test.asm
+++ test.asm
@@ -1,11 +1,19 @@
-lda #5 ; 5-ot mint konstans A-ba
-ldx #0;
-.define SCREEN $400
-; loop body
-loop:
-    sta %SCREEN, X
-    inx
-    cpx #10
-    bne loop ; loop until x is 10
+;lda #5 ; 5-ot mint konstans A-ba
+;ldx #0;
+;.define SCREEN $400
+;; loop body
+;loop:
+;    sta %SCREEN, X
+;    inx
+;    cpx #10
+;    bne loop ; loop until x is 10
+;rts
 
-rts
+; goal of today: make this compile and work
+
+.org $c000
+.define BORDER $D020
+.define test 5
+loop:
+    inc @BORDER
+    jmp &loop
