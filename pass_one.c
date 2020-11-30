2f7a5be71414e795256a525e3867f44a46f2414d
Sasszem <barath.laszlo.szolnok@gmail.com>
2020-11-30 16:10:24 +0100

    refactor + doc: pass_one

--- pass_one.c
+++ pass_one.c
@@ -9,90 +9,100 @@
 #include "tokenFunc.h"
 
 
-/**
- * @brief Compilation pass 1
- * @param s compiler state
- * @param tokens all tokens loaded so far
- * @returns 0 on success, -1 on error
- */
-int pass_one(State *s) {
-    TokensListElement *ptr = s->tokens->head;
+int pass_one(State* s) {
+    TokensListElement* ptr = s->tokens->head;
+    
+    // stack to store state of conditionals
+    // top of stack is the current value
+    // 1 means we disabled compilation
+    // in that case we do not generate anything
     istack_ptr ifstack = istack_new();
     if (!ifstack) {
         FAIL("Pass 1 init failed!\n");
         return -1;
     }
+    // skip ONE token
+    // i.e. drop current one
     int skiponce = 0;
-    enum PPCommand p;
-    while (ptr!=NULL) {
-        if (ptr->token->type == TT_PREPROC) {
-            p = do_preprocessor_token(s, ptr, istack_top(ifstack, 0));
-            skiponce = 1;
-            if (p==PPC_IF_TRUE) {
-                if (istack_push(ifstack, istack_top(ifstack, 0))<0) {
-                    FAIL("Pass 1 failed!\n");
-                    istack_free(ifstack);
-                    return -1;
-                }
-            }
-            if (p==PPC_IF_FALSE) {
-                if (istack_push(ifstack, 1)<0) {
-                    FAIL("Pass 1 failed!\n");
-                    istack_free(ifstack);
-                    return -1;
-                }
-            }
 
-            if (p==PPC_ENDIF) {
-                if (istack_pop(ifstack)<0) {
+    // return value from directive processing
+    enum DIRCommand p;
+
+    // on all tokens
+    while (ptr != NULL) {
+        
+        ///////////////
+        // DIRECTIVE //
+        ///////////////
+        if (ptr->token->type == TT_DIRECTIVE) {
+            p = do_directive_token(s, ptr, istack_top(ifstack, 0));
+            skiponce = 1;
+
+            if (p == DIR_IF_TRUE)
+                if (istack_push(ifstack, istack_top(ifstack, 0)) < 0) goto ERR_FREE;
+            if (p == DIR_IF_FALSE)
+                if (istack_push(ifstack, 1) < 0) goto ERR_FREE;
+
+            if (p == DIR_ENDIF)
+                if (istack_pop(ifstack) < 0) {
                     ERROR("More endif's than if's!\n");
                     token_print(ptr->token);
-                    istack_free(ifstack);
-                    FAIL("Pass 1 failed!\n");
-                    return -1;
+                    goto ERR_FREE;
                 }
-            }
-            if (p==PPC_STOP) {
-                FAIL("Pass 1 failed!\n");
-                return -1;
-            }
+            if (p == DIR_STOP) goto ERR_FREE;
         }
+
+        /////////////////
+        // INSTRUCTION //
+        /////////////////
         if (ptr->token->type == TT_INSTR) {
             ptr->token->instr.address = s->PC;
-            if (token_analyze_instruction(s, ptr->token)<0) {
-                FAIL("Pass 1 failed!\n");
-                return -1;
-            }
-            if (token_get_operand(s, ptr->token)<0) {
-                FAIL("Pass 1 failed!\n");
-                return -1;
-            }
+
+            if (token_analyze_instruction(s, ptr->token) < 0) goto ERR_FREE;
+            if (token_get_operand(s, ptr->token) < 0) goto ERR_FREE;
+
             LOGDO(3, token_print(ptr->token));
             LOG(3, "Instruction: \n");
             LOGDO(3, instruction_print(ptr->token->instr.inst));
             LOG(3, "A-mode: %s\n", ADRM_NAMES[ptr->token->instr.addressmode]);
         }
+
+        ///////////
+        // LABEL //
+        ///////////
         if (ptr->token->type == TT_LABEL) {
             char labelname[MAP_MAX_KEY_LEN];
-            strncpy(labelname, ptr->token->stripped, ptr->token->len-1);
-            labelname[ptr->token->len-1] = 0;
+            strncpy(labelname, ptr->token->stripped, ptr->token->len - 1);
+            labelname[ptr->token->len - 1] = 0;
+
             LOG(5, "Label: %s\n", labelname);
-            if (map_find(s->labels, labelname)) {
+            if (map_get(s->labels, labelname)>=0) {
                 ERROR("Can not re-define label '%s'\n", labelname);
                 token_print(ptr->token);
-                return -1;
+                goto ERR_FREE;
             }
+
             map_set(s->labels, labelname, s->PC);
             LOG(3, "Set label: '%s' to %d\n", labelname, s->PC);
             skiponce = 1;
         }
-        s->PC += ptr->token->binSize;
-        if (istack_top(ifstack, 0)||skiponce){
+
+        ////////////
+        // COMMON //
+        ////////////
+        if (istack_top(ifstack, 0) || skiponce) {
             ptr = tokenslist_remove(s->tokens, ptr);
             skiponce = 0;
         } else {
+            s->PC += ptr->token->binSize;
             ptr = ptr->next;
         }
     }
     return 0;
+
+
+ERR_FREE:
+    FAIL("Pass 1 failed!\n");
+    istack_free(ifstack);
+    return -1;
 }
--- pass_one.h
+++ pass_one.h
@@ -4,6 +4,13 @@
 #include "tokenslist.h"
 #include "state.h"
 
+/**
+ * @brief Compilation pass 1
+ * @param s compiler state
+ * @returns 0 on success, -1 on error
+ * 
+ * See documentation on pass 1 for further info!
+ */
 int pass_one(State *s);
 
 #endif
