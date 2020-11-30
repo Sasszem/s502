7aaa3a0227c171005d6418b32f38de08997e665f
Sasszem <barath.laszlo.szolnok@gmail.com>
2020-11-30 13:08:19 +0100

    doc: number

--- number.c
+++ number.c
@@ -2,18 +2,7 @@
 #include <string.h>
 #include "logging.h"
 
-int is_valid_define_char(char c) {
-    return  ('a'<=c && c<='z') ||
-            ('A'<=c && c<='Z') ||
-            ('0'<=c && c<='9') ||
-            c=='_';
-}
 
-/**
- * Convert a hex char to a digit
- * Handles upper or lowercase chars
- * Returns NUMBER_ERROR on failed conversion
- */
 int number_char_to_digit(char c) {
     if ('0'<=c && c<='9')
         return c-'0';
@@ -24,11 +13,6 @@ int number_char_to_digit(char c) {
     return NUMBER_ERROR;
 }
 
-/**
- * Parse a number from a string
- * Handles base marker $ symbol
- * Returns NUMBER_ERROR on failed parsing
- */
 int number_parse_number(char *str, int count) {
     int base = 10;
     int ptr = 0;
@@ -51,12 +35,7 @@ int number_parse_number(char *str, int count) {
     return num;    
 }
 
-/**
- * interpret a string as a constant, label or number
- * - get the value of a constant
- * - parse number
- * Returns NUMBER_ERROR on error, or NUMBER_LABEL_NODEF if number is a valid label with no value
- */
+
 int number_get_number(State *s, char *str, int count) {
     int ptr = 0;
     if (str[ptr]=='#')
--- number.h
+++ number.h
@@ -8,7 +8,41 @@ enum {
     NUMBER_LABEL_NODEF = -2,
 };
 
+/**
+ * @brief interpret a string as a constant, label or number
+ * @param s State to read constant / label values from
+ * @param str string to interpret
+ * @param count length of string to parse
+ * 
+ * String is not required to be 0-terminated.    
+ * - get the value of a constant or label   
+ * - parse number   
+ * - handles modifier symbols
+ * Returns NUMBER_ERROR on error, or NUMBER_LABEL_NODEF if number is a valid label with no value   
+ */
 int number_get_number(State *s, char *str, int count);
+
+/**
+ * @brief Parse a number from a string
+ * @returns value or NUMBER_ERROR on failed parsing
+ * @param str string to parse
+ * @param count length of string to parse
+ * 
+ * Only parses numbers, returns error on labels, defines, etc.
+ * Handles base marker $ symbol   
+ * String is not needed to be 0-terminated
+ */
 int number_parse_number(char *str, int count);
+
+
+/**
+ * @brief Convert a hex char to a digit
+ * @param c char to convert
+ * @returns value or NUMBER_ERROR on some error
+ * 
+ * Handles upper or lowercase chars
+ * Returns NUMBER_ERROR on failed conversion
+ */
 int number_char_to_digit(char c);
+
 #endif
