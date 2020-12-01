0d78ed5418b8af8ce449baa887513ff20087232a
Sasszem <barath.laszlo.szolnok@gmail.com>
2020-12-01 11:22:40 +0100

    Update example

---
+++ c64.asm
@@ -0,0 +1,7 @@
+; just a few C64 memory locations 
+.org $FFD2
+CHROUT:
+.org $D020
+BORDER:
+.org $D021
+BACKGROUND:
--- test.asm
+++ test.asm
@@ -1,19 +1,47 @@
-;lda #5 ; 5-ot mint konstans A-ba
-;ldx #0;
-;.define SCREEN $400
-;; loop body
-;loop:
-;    sta %SCREEN, X
-;    inx
-;    cpx #10
-;    bne loop ; loop until x is 10
-;rts
+; Implements a super-simple program for the C=64
+; (changes border color in in infinite loop with some busy wait loops)
+; creates a PRG file structure (see link bellow), so the result can be just dropped into an emulator (for ex. VICE)
+; or written to a 5/4' floppy if you want
+
+; PRG file:             https://michaelcmartin.github.io/Ophis/book/x72.html
+; C=64 memory map:      http://sta.c64.org/cbm64mem.html
+; C=64 KERNAL rom:      https://sta.c64.org/cbm64krnfunc.html
+; C=64 clear screen:    http://www.rarityguide.com/forums/commodore-basic-classroom/218-how-do-i-clear-screen-commodore-64-a.html
+; C=64 BASIC tokens:    https://www.c64-wiki.com/wiki/BASIC_token
+
+.include c64.asm
+
+.data w:$0801 ; PRG file header
+
+.org $0801 ; we work in BASIC program memory
+
+; 1st (and only) BASIC line
+.data w:&lastline w:10 $9e "2064" 0
+; next line PTR, line number, BASIC token (sys), rest of line and trailing 0
+
+lastline:
+.data w:0
+; NULL as next line ptr indicates end of program
+
+.pad $810 0
+; we want to start at 2064
+
+
+begin:
+; clear screen
+lda #147
+jsr &CHROUT
 
 ; goal of today: make this compile and work
 
-.org $c000
-.define BORDER $D020
-.define test 5
+
+; Funny effect
+
+.define WAIT 32
 loop:
-    inc @BORDER
+    inx
+    cpx #@WAIT
+    bne &loop
+
+    inc &BORDER
     jmp &loop
