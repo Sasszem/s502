73ee575741288e2abdaaae215824a3b5bf42d7df
Sasszem <barath.laszlo.szolnok@gmail.com>
2020-12-01 11:27:48 +0100

    add utils_split_string

--- util.c
+++ util.c
@@ -1,3 +1,6 @@
+#include "debugmalloc.h"
+#include <string.h>
+#include <stdlib.h>
 #include "util.h"
 #include "logging.h"
 
@@ -20,4 +23,35 @@ int util_match_char(char a, char b) {
             ) && 
             (a-b=='A'-'a' || a-b=='a'-'A')
         );
+}
+
+
+char **util_split_string(char *str, int*n) {
+    // function to split a string into substrings on spaces
+    // uses an ugly trick to avoid multiple buffers
+    // (we NEED to store a copy of the original data bc we must write 0 terminators)
+    int l = strlen(str);
+
+    // count segments
+    int m = 1;
+    for (int i = 0; i<l; i++)
+        if (str[i]==' ') m++;
+    *n = m; 
+
+    // the buffers stores the *char[]-s and then the copy of the data
+    char **r = malloc(sizeof(char*)*m + l + 1);
+
+    char *buf = (char*)r + sizeof(char*)*m;
+    strcpy(buf, str); 
+    
+    // first segemtn is the begining of the string
+    r[0] = buf;
+    // fill the table and write 0 terminators
+    int j = 1;
+    for (int i = 0; i<l; i++)
+        if (str[i]==' ') {
+            buf[i] = 0;
+            r[j++] = buf+i+1;
+        }
+    return r;
 }
--- util.h
+++ util.h
@@ -4,4 +4,18 @@
 char* util_find_string_segment(char *ptr);
 int util_match_char(char a, char b);
 
+
+
+/**
+ * @brief split a string into segments on spaces
+ * @param str string to split
+ * @param n int pointer to return segment count to
+ * @returns an array of substrings
+ * 
+ * Uses a not-so-nice trick to enable easier handling    
+ * The returned *char[] also contains a buffer with the actual substring data   
+ * This also means simply freeing it is all what is needed to be done on the callee side   
+ */
+char **util_split_string(char *str, int*n);
+
 #endif
