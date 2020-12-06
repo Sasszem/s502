37598797eb8e19052ed7604f7811d99e011eb4bb
Sasszem <barath.laszlo.szolnok@gmail.com>
2020-12-06 11:13:08 +0100

    Add final example

--- c64.asm
+++ c64.asm
@@ -1,7 +1,53 @@
 ; just a few C64 memory locations 
+;
+; based on:
+;
+; C=64 memory map:      http://sta.c64.org/cbm64mem.html
+; C=64 KERNAL rom:      https://sta.c64.org/cbm64krnfunc.html
+
+
+; zeropage 4-byte buffer
+; quite useful for indirect addressing
+; and also safe to use
+.org $fb
+C64_ZPGM1:
+.org $fc
+C64_ZPGM2:
+.org $fd
+C64_ZPGM3:
+.org $fe
+C64_ZPGM4:
+
+; default screen memory (1000 bytes for 40*25)
+.org $400
+C64_SCREEN:
+
+; currently pressed key
+.org 197
+C64_KEY:
+
+; kernal CHROUT routine
 .org $FFD2
-CHROUT:
+C64_CHROUT:
+; kernal VIC init routine
+.org $ff81	
+C64_SCINIT:
+
+; VIC II border color
 .org $D020
-BORDER:
+C64_BORDER:
+
+; VIC II background color
 .org $D021
-BACKGROUND:
+C64_BACKGROUND:
+
+; VIC II raster line register - can be used to set interrupt
+.org $D012
+C64_RASTLINE:
+
+; VIC II interrupr registers
+.org $D019
+C64_INTST:
+.org $D01A
+C64_INTCTRL:
+
--- test.asm
+++ test.asm
@@ -4,12 +4,14 @@
 ; or written to a 5/4' floppy if you want
 
 ; PRG file:             https://michaelcmartin.github.io/Ophis/book/x72.html
-; C=64 memory map:      http://sta.c64.org/cbm64mem.html
-; C=64 KERNAL rom:      https://sta.c64.org/cbm64krnfunc.html
-; C=64 clear screen:    http://www.rarityguide.com/forums/commodore-basic-classroom/218-how-do-i-clear-screen-commodore-64-a.html
 ; C=64 BASIC tokens:    https://www.c64-wiki.com/wiki/BASIC_token
 
 .include c64.asm
+
+
+; #############################
+; # PRG FILE AND BASIC LOADER #
+; #############################
 
 .data w:$0801 ; PRG file header
 
@@ -26,22 +28,239 @@ lastline:
 .pad $810 0
 ; we want to start at 2064
 
+; ##################
+; # ACTUAL PROGRAM #
+; ##################
 
-begin:
 ; clear screen
+; from http://www.rarityguide.com/forums/commodore-basic-classroom/218-how-do-i-clear-screen-commodore-64-a.html
 lda #147
-jsr &CHROUT
+jsr &C64_CHROUT
 
-; goal of today: make this compile and work
+; print text
+ldx #>&BEGINTEXT
+ldy #<&BEGINTEXT
+jsr &print
 
+; ##################
+; # INITIAL SCREEN #
+; ##################
 
-; Funny effect
-
-.define WAIT 32
+; wait screen with fancy border effect 
 loop:
+    lda &C64_BORDER
     inx
-    cpx #@WAIT
+    ;adc #0 ; if X overflow we add 1 from carry
+    sta &C64_BORDER
+    ; check if spacebar is pressed
+    lda &C64_KEY
+    cmp #60
     bne &loop
 
-    inc &BORDER
-    jmp &loop
+; gray initial border
+lda #11
+sta &C64_BORDER
+; black background
+lda #0
+sta &C64_BACKGROUND
+; white text
+lda #5
+jsr &C64_CHROUT
+
+; clear it again
+lda #147
+jsr &C64_CHROUT
+
+; ####################
+; # ACTUAL BALL DEMO #
+; ####################
+
+.define WAIT 3000
+gameloop:
+    ; we use a 16 bit counter
+    ; and only run logic if it's at a spefific value
+    ; but still check exit condition regardless of its value 
+
+; 16 bit increment
+    inc &WAITCOUNTER
+    bne &gameloop_wait_compare
+    inc &WAITCOUNTER_H
+
+gameloop_wait_compare:
+; 16 bit comare
+    lda #<@WAIT
+    cmp &WAITCOUNTER
+    bne &gameloop_keycheck
+    lda #>@WAIT
+    cmp &WAITCOUNTER_H
+    bne &gameloop_keycheck
+
+; following is "game" logic
+
+; reset counter
+    lda #0
+    sta &WAITCOUNTER
+    sta &WAITCOUNTER_H
+
+gameloop_undraw:
+    ; un-draw the ball
+    jsr &make_balladdr
+    lda #$20
+    ldx #0
+    sta (&C64_ZPGM1, X)
+
+; update X position and flip velocity if we reached a border
+gameloop_x:
+    ; add VX to X
+    lda &BALLX
+    clc
+    adc &BALLVX
+    sta &BALLX
+    ; if at 0
+    beq &gameloop_flipX
+    ; if at last column
+    cmp #39
+    beq &gameloop_flipX
+    jmp &gameloop_y ; skip flipping
+
+gameloop_flipX:
+    lda #0
+    sec
+    sbc &BALLVX
+    sta &BALLVX
+    inc &C64_BORDER
+
+; update Y position and velocity
+; kinda the same deal as with X 
+gameloop_y:
+    ; add VX to X
+    lda &BALLY
+    clc
+    adc &BALLVY
+    sta &BALLY
+    ; if at 0
+    beq &gameloop_flipY
+    ; if at last row
+    cmp #24
+    beq &gameloop_flipY
+    jmp &gameloop_draw ; skip flipping
+
+gameloop_flipY:
+    lda #0
+    sec
+    sbc &BALLVY
+    sta &BALLVY
+    inc &C64_BORDER
+
+gameloop_draw:
+    ; draw the ball
+    jsr &make_balladdr
+    lda #87
+    ldx #0
+    sta (&C64_ZPGM1, X)
+
+gameloop_keycheck:
+    ; check if Q is pressed
+    lda 197
+    cmp #62
+    bne &gameloop
+
+; ####################
+; # CLEANUP AND EXIT #
+; ####################
+
+; undo everything to the screen
+jsr &C64_SCINIT
+
+; print text
+ldx #>&BYETEXT
+ldy #<&BYETEXT
+jsr &print
+
+rts
+
+; #############
+; # VARIABLES #
+; #############
+
+; wait counter to slow down the main loop of the program 
+WAITCOUNTER:
+.data 0
+WAITCOUNTER_H:
+.data 0
+; damn I miss offsetting...
+
+; ball position and velocity
+BALLX:
+.data 5
+BALLY:
+.data 3
+BALLVX:
+.data 1
+BALLVY:
+.data 1
+
+; text to print at the beginning
+BEGINTEXT:
+.data "S502" 32 "ASSEMBLER" 32 "SANITY" 32 "TEST!" $d $d
+.data "PREESS" 32 "SPACE" 32 "TO" 32 "START!" $d
+.data "(PRESS" 32 "Q" 32 "TO" 32 "QUIT" 32  "WHEN" 32 "STARTED)" $D 0
+
+; text to print at the end
+BYETEXT:
+.data 147 "GOODBYE!" 0
+
+; # ##########
+; # ROUTINES #
+; ############
+
+; print a string using CHROUT
+; address should be in y:x
+; based on https://www.c64-wiki.com/wiki/Assembler_Example
+print:
+    sty *&C64_ZPGM1
+    stx *&C64_ZPGM2
+    ldy #$0
+print_loop:
+    lda ($fb), y
+    beq &print_quit
+    jsr &C64_CHROUT
+    iny
+    jmp &print_loop
+print_quit:
+    RTS
+
+; set up address of ball (in screen memory) in zeropage buffer
+; has no parameters
+; calculates SCREEN + 40*Y + X 
+make_balladdr:
+    ; set it to SCREEN
+    lda #<&C64_SCREEN
+    sta *&C64_ZPGM1
+    lda #>&C64_SCREEN
+    sta *&C64_ZPGM2
+    ldx &BALLY
+make_balladdr_addloop:
+    cpx #0
+    beq &make_balladdr_addx
+    dex
+    lda *&C64_ZPGM1
+    clc
+    adc #40
+    sta *&C64_ZPGM1
+    bcc &make_balladdr_addloop
+    inc *&C64_ZPGM2
+    jmp &make_balladdr_addloop
+
+make_balladdr_addx: 
+    lda *&C64_ZPGM1
+    clc
+    adc &BALLX
+    sta *&C64_ZPGM1
+    bcc &make_balladdr_exit
+    inc *&C64_ZPGM2
+
+make_balladdr_exit:
+    rts
+
+set_ball:
