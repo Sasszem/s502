f703127ac7bddf864433329db1e807d0417772f6
Sasszem <barath.laszlo.szolnok@gmail.com>
2020-12-07 01:02:49 +0100

    doc + format: number

--- src/number.c
+++ src/number.c
@@ -4,6 +4,11 @@
 #include "logging.h"
 
 
+/**
+ * @file
+ * @brief implement number module functions
+ */
+
 int number_char_to_digit(char c) {
     if ('0' <= c && c <= '9')
         return c - '0';
@@ -14,6 +19,14 @@ int number_char_to_digit(char c) {
     return NUMBER_ERROR;
 }
 
+
+/**
+ * @brief interpret a string as a decimal or hex number
+ * @param str string to interpret
+ * @returns string as a number or NUMBER_ERROR
+ *
+ * This one only parses decimals and hexes without any modifiers
+ */
 int number_parse_number(char* str) {
     int count = strlen(str);
 
@@ -38,7 +51,15 @@ int number_parse_number(char* str) {
     return num;
 }
 
-int number_get_raw(State *s, char* str) {
+/**
+ * @brief interpret number without modifiers
+ * @param s state to read from
+ * @param str string to interpret
+ *
+ * Resolves labels & constants, parses numbers<br>
+ * Does not interpret modifiers
+ */
+int number_get_raw(State* s, char* str) {
     if (str[0] == '@' || str[0] == '&') {
         Map* m = str[0] == '@' ? s->defines : s->labels;
         int n = map_get(m, &str[1]);
@@ -53,13 +74,13 @@ int number_get_raw(State *s, char* str) {
 
 int number_get_number(State* s, char* str) {
 
-    int p = (str[0]=='>' ||str[0]=='<')? 1:0;
+    int p = (str[0] == '>' || str[0] == '<') ? 1 : 0;
     int num = number_get_raw(s, &str[p]);
-    
-    if (num<0) return num;
 
-    num = str[0] == '>' ? num>>8 : num;
-    num &= str[0]=='>' || str[0]=='<' ? 0xff : 0xffff;
+    if (num < 0) return num;
+
+    num = str[0] == '>' ? num >> 8 : num;
+    num &= str[0] == '>' || str[0] == '<' ? 0xff : 0xffff;
 
     return num;
 }
--- src/number.h
+++ src/number.h
@@ -3,8 +3,17 @@
 
 #include "state.h"
 
+/**
+ * @file
+ * @brief Number module to parse numbers
+ *
+ * Handles number in decimal and hex, labels, constants and modifiers
+ */
+
 enum {
+    /// Could not parse a number or constant is undefined
     NUMBER_ERROR = -1,
+    /// Undefined label
     NUMBER_LABEL_NODEF = -2,
 };
 
@@ -12,25 +21,21 @@ enum {
  * @brief interpret a string as a constant, label or number
  * @param s State to read constant / label values from
  * @param str string to interpret
- * @param count length of string to parse
  *
- * String is not required to be 0-terminated.
  * - get the value of a constant or label
  * - parse number
- * - handles modifier symbols
+ * - handles modifier symbols<br>
  * Returns NUMBER_ERROR on error, or NUMBER_LABEL_NODEF if number is a valid label with no value
  */
 int number_get_number(State* s, char* str);
 
-
-int number_parse_number(char* str);
 
 /**
  * @brief Convert a hex char to a digit
  * @param c char to convert
  * @returns value or NUMBER_ERROR on some error
  *
- * Handles upper or lowercase chars
+ * Handles upper or lowercase chars<br>
  * Returns NUMBER_ERROR on failed conversion
  */
 int number_char_to_digit(char c);
